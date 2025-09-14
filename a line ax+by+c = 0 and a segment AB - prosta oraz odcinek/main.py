f3e1f2f430669bec1682ca5ad755bdb5bb0eeee0
ProjectsTaras <tymkivtaras17@gmail.com>
2025-09-14 22:37:12 +0200

    add a line ax+by+c and a segment AB - prosta i odcinek

---
+++ a line ax+by+c = 0 and a segment AB - prosta oraz odcinek/main.py
@@ -0,0 +1,17 @@
+A = float(input("Podaj A: "))
+B = float(input("Podaj B: "))
+C = float(input("Podaj C: "))
+
+xA = float(input("Podaj xA: "))
+yA = float(input("Podaj yA: "))
+xB = float(input("Podaj xB: "))
+yB = float(input("Podaj yB: "))
+
+# Sprawdzenie
+warunekA = A * xA + B * yA + C
+warunekB = A * xB + B * yB + C
+
+if warunekA == 0 and warunekB == 0:
+    print("Odcinek |AB| leży na prostej.")
+else:
+    print("Odcinek |AB| NIE leży na prostej.")
