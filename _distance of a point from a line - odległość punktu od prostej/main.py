a66c8f19669b6a33c2596fe79cf34cc21ac51096
ProjectsTaras <tymkivtaras17@gmail.com>
2025-09-14 21:29:56 +0200

    distance of a point from a line

---
+++ _distance of a point from a line - odległość punktu od prostej/main.py
@@ -0,0 +1,11 @@
+import math
+
+A = float(input("Podaj A: "))
+B = float(input("Podaj B: "))
+C = float(input("Podaj C: "))
+x0 = float(input("Podaj x0: "))
+y0 = float(input("Podaj y0: "))
+
+d = abs(A * x0 + B * y0 + C) / math.sqrt(A**2 + B**2)
+
+print(f"Odległość punktu P od prostej wynosi: {d:.2f}")
