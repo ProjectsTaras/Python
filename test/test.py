007f7255ec06812a027a821cc61476493766db15
ProjectsTaras <tymkivtaras17@gmail.com>
2025-10-05 22:12:14 +0200

    test i mod

---
+++ test/test.py
@@ -0,0 +1,14 @@
+print(1%7)
+print(2%7)
+print(3%7)
+print(4%7)
+print(5%7)
+print(6%7)
+print(7%7)
+
+#dzieli wprowadzony ciąg na części za pomocą spacji
+print("3.4 5".split())
+print("3 5".split()[0])
+print("3 5".split()[1])
+
+# print(map(float, ["3", "5"])) → [3.0, 5.0]
