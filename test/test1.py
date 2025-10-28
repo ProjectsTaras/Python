dbcc2db76ff9572f397070cad878cbff501d7f44
ProjectsTaras <tymkivtaras17@gmail.com>
2025-10-28 20:09:32 +0100

    sortfilesinfolders

--- test/test1.py
+++ test/test1.py
@@ -1,1 +1,38 @@
-print("ggg")
+import math
+
+print("podaj wspolczynnik A prostej", end=" ")
+
+A = float(input())
+
+print("podaj wspolczynnik B prostej", end=" ")
+
+B = float(input())
+
+print("podaj wspolczynnik C prostej", end=" ")
+
+C = float(input())
+
+print("podaj wspolrzedna x punktu A", end=" ")
+
+xa = float(input())
+
+print("podaj wspolrzedna y punktu A", end=" ")
+
+ya = float(input())
+
+print("podaj wspolrzedna x punktu B", end=" ")
+
+xb = float(input())
+
+print("podaj wspolrzedna y punktu B", end=" ")
+
+yb = float(input())
+
+lezy = A*xa + B*ya + C
+lezy2 = A*xb + B*yb + C
+
+if lezy == 0 and lezy2 == 0:
+    print("odcinek lezy na prostej")
+
+else:
+    print("odcinek nie lezy na prostej")
